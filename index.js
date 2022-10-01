a5073132656b0b5986c844e2bcce80d031e8db1b
Omar Brikaa <brikaaomar@gmail.com>
2022-10-01 23:02:35 +0200

    Use mutation observer

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-console.log("BlackBoard Skipper")
-Array.from(document.getElementsByTagName('A'))
-    .filter((a) => a.href.includes('bbcswebdav'))
-    .forEach((a) => {
-        const new_anchor = document.createElement('a');
-        new_anchor.href = a.href;
-        new_anchor.innerHTML = a.innerHTML;
-        a.parentElement.replaceChild(new_anchor, a);
-    });
+const callback = (_, observer) => {
+    if (document.getElementById('content')) {
+        observer.disconnect();
+        Array.from(document.getElementsByTagName('A'))
+            .filter((a) => a.href.includes('bbcswebdav'))
+            .forEach((a) => {
+                const new_anchor = document.createElement('a');
+                new_anchor.href = a.href;
+                new_anchor.innerHTML = a.innerHTML;
+                a.parentElement.replaceChild(new_anchor, a);
+            });
+        observer.observe(document, config);
+    }
+};
+const config = { childList: true, subtree: true };
+const observer = new MutationObserver(callback);
+observer.observe(document, config);
