848b8122aca6c0b3f4c414d7e99e1494720436ff
Omar Brikaa <brikaaomar@gmail.com>
2022-10-02 11:41:54 +0200

    No need for mutation observer, just all_frames

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const callback = (_, observer) => {
-    if (document.getElementById('content')) {
-        observer.disconnect();
-        Array.from(document.getElementsByTagName('A'))
-            .filter((a) => a.href.includes('bbcswebdav'))
-            .forEach((a) => {
-                const new_anchor = document.createElement('a');
-                new_anchor.href = a.href;
-                new_anchor.innerHTML = a.innerHTML;
-                a.parentElement.replaceChild(new_anchor, a);
-            });
-        observer.observe(document, config);
-    }
-};
-const config = { childList: true, subtree: true };
-const observer = new MutationObserver(callback);
-observer.observe(document, config);
+Array.from(document.getElementsByTagName('A'))
+    .filter((a) => a.href.includes('bbcswebdav'))
+    .forEach((a) => {
+        const new_anchor = document.createElement('a');
+        new_anchor.href = a.href;
+        new_anchor.innerHTML = a.innerHTML;
+        a.parentElement.replaceChild(new_anchor, a);
+    });
